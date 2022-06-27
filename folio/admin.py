e183a65be5e2b676d2fd642839fa5d29aa1b5de5
nancyngunjiri1 <nancyngunjiri1@gmail.com>
2022-06-27 13:11:17 +0300

    register all my models

--- folio/admin.py
+++ folio/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import PersonalInformation, Projects, About
 
 # Register your models here.
+class PersonalInformationAdmin(admin.ModelAdmin):
+    list_display = ('name_complete')
+    search_fields = ["name_complete"]
+
+admin.site.register(PersonalInformation)
+admin.site.register(About)
+admin.site.register(Projects)
