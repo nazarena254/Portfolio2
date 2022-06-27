a9c4d552c6622f239caa9ec35ed71ce3c70a8e1d
nancyngunjiri1 <nancyngunjiri1@gmail.com>
2022-06-27 13:09:51 +0300

    create views for my index page

--- folio/views.py
+++ folio/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+def home_page(request):
+    myinfo = PersonalInformation.objects.all()
+    myabout = About.objects.all()
+    projects = Projects.objects.all()
+    context = {
+        "info": myinfo,
+        "about": myabout,
+        "projects": projects,
+    }
+    return render(request, 'index.html', context)
+
