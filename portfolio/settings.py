12fddeade18bb424797e372a856d76c827c3cb5a
nancyngunjiri1 <nancyngunjiri1@gmail.com>
2022-07-01 21:21:08 +0300

    fix image error

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -135,7 +135,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_URL = '/images/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
